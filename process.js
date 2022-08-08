fdf9b8bb9f04606efc66197bd260ec25a168bead
Rui Vergani Neto <ruiverganineto@gmail.com>
2022-08-08 16:37:45 -0300

    using process nodeJS

---
+++ process.js
@@ -0,0 +1,5 @@
+console.log(process);
+
+console.log(process.argv);
+
+console.log('Seu nome e: ', process.argv[2] + ' ' + process.argv[3]);
--- require.js
+++ require.js
@@ -2,3 +2,7 @@
 const path = require('path'); // objeto
 
 console.log(path.basename(__filename)); // nome base do arquivo (require.js)
+
+// My new modules
+const myModule = require('./exports');
+console.log(myModule);
