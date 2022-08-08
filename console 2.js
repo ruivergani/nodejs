3c2b2eb35e02b6e9b0481572479e4b94a7f9b599
Rui Vergani Neto <ruiverganineto@gmail.com>
2022-08-08 16:02:14 -0300

    using modules in nodeJS

---
+++ console 2.js
@@ -0,0 +1,2 @@
+// Diretorio local, onde esta o arquivo que estamos executando
+consol(e.log)
---
+++ require.js
@@ -0,0 +1,4 @@
+// Requerido
+const path = require('path'); // objeto
+
+console.log(path.basename(__filename)); // nome base do arquivo (require.js)
